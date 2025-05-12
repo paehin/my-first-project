769871f1905f8d104767f1d9e9bfcd63106fbe89
paehin <pkswiftads@gmail.com>
2025-05-12 15:29:59 +0000

    finished the script code

--- script.js
+++ script.js
@@ -4,5 +4,29 @@ document.getElementById("contact-form").addEventListener('submit', function(even
     const name = document.getElementById("name").value.trim()
     const email = document.getElementById("email").value.trim()
     const message = document.getElementById("message").value.trim()
-    const errorMessage = document.getElementById("error-message")
-})
+
+
+    if (!name) {
+        alert('Please enter your name.')
+        return;
+    }
+
+    if (!email) {
+        alert('Please enter your email.')
+        return;
+    }
+
+    if (!message) {
+        alert('Please enter your message.')
+        return;
+    }
+
+    // If al validations pass, show success message
+    alert('Thank you for your message!');
+    this.reset();
+});
+
+function validateEmail(email) {
+    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+    return re.test(String(email).toLowerCase())
+}
